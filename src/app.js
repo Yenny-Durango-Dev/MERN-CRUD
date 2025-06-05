a86d9616ee01d53b1e6363945eac9b2fd1a994f4
Yenny-Durango-Dev <yennycarolinadurango@gmail.com>
2025-06-05 12:02:10 -0500

    Tercer commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import authRoutes from './routes/auth.routes.js'; // Importar las rutas de autenticación
+
 const app = express();
 
 app.use(morgan('dev')); // Middleware para registrar las peticiones HTTP en la consola
+
+app.use(express.json()); // Middleware para parsear el cuerpo de las peticiones como JSON
+
+app.use("/api",authRoutes); // Usar las rutas de autenticación
 
 export default app; // Exportar app para que pueda ser utilizado en otros archivos
