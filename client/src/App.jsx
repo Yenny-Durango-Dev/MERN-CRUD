74c0df86451d8bf647bc9f547c380fa991912e03
Yenny-Durango-Dev <yennycarolinadurango@gmail.com>
2025-06-06 17:32:01 -0500

    Octava modificacion c:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Welcome to the Home Page</h1>}/>
+        <Route path='/login' element={<h1>Login</h1>}/>
+        <Route path='/register' element={<h1>Register</h1>}/>
+        <Route path='/tasks' element={<h1>Task page</h1>}/>
+        <Route path='/add-task' element={<h1>New page</h1>}/>
+        <Route path='/tasks/:id' element={<h1>Update Task</h1>}/>
+        <Route path='/profile' element={<h1>Profile</h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App;
