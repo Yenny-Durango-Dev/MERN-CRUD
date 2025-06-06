7fabc3f983e58a4881950ef1af1dc1b4e22f2c71
Yenny-Durango-Dev <yennycarolinadurango@gmail.com>
2025-06-06 12:42:10 -0500

    Septima modificacion c:

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,10 @@
+export const validateSchema = (schema) => (req, res, next) => {
+    try {
+        schema.parse(req.body); // Validar el cuerpo de la solicitud contra el esquema proporcionado
+        next();
+    } catch (error) {
+        return res
+            .status(400)
+            .json({ error: error.errors.map((error) => error.message) }); // Si hay un error de validación, enviar un mensaje de error
+    }
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { login, register, logout, profile } from "../controllers/auth.controllers.js";
 import { authRequired } from "../middlewares/validateToken.js"; // Importar el middleware de autenticación
+import { validateSchema } from "../middlewares/validator.middleware.js"
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js"
+
 const router = Router();
 
-router.post('/login', login);
-router.post('/register', register);
+router.post('/register', validateSchema(registerSchema), register);
+
+router.post('/login', validateSchema(loginSchema), login);
+
 router.post('/logout', logout);
+
 router.get('/profile', authRequired, profile);
 
 export default router; // Exportar el router para que pueda ser utilizado en otros archivos
