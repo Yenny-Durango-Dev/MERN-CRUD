0efd352fc2837396a50bb06f6110cabbbd142790
Yenny-Durango-Dev <yennycarolinadurango@gmail.com>
2025-06-09 16:20:59 -0500

    Novena modificacion c:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
+import RegisterPage from "./pages/registerPage";
+import LoginPage from "./pages/loginPage";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<h1>Welcome to the Home Page</h1>}/>
-        <Route path='/login' element={<h1>Login</h1>}/>
-        <Route path='/register' element={<h1>Register</h1>}/>
-        <Route path='/tasks' element={<h1>Task page</h1>}/>
-        <Route path='/add-task' element={<h1>New page</h1>}/>
-        <Route path='/tasks/:id' element={<h1>Update Task</h1>}/>
-        <Route path='/profile' element={<h1>Profile</h1>}/>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1>Welcome to the Home Page</h1>} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/tasks' element={<h1>Task page</h1>} />
+          <Route path='/add-task' element={<h1>New page</h1>} />
+          <Route path='/tasks/:id' element={<h1>Update Task</h1>} />
+          <Route path='/profile' element={<h1>Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 export default App;
--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 import authRoutes from './routes/auth.routes.js'; // Importar las rutas de autenticación
 import taskRoutes from './routes/tasks.routes.js'; // Importar las rutas de tareas
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5174',
+}));
 app.use(morgan('dev')); // Middleware para registrar las peticiones HTTP en la consola
 
 app.use(express.json()); // Middleware para parsear el cuerpo de las peticiones como JSON
 
 app.use(cookieParser()); // Middleware para parsear las cookies de las peticiones
 
-app.use("/api",authRoutes); // Usar las rutas de autenticación
+app.use("/api", authRoutes); // Usar las rutas de autenticación
 app.use("/api", taskRoutes); // Usar las rutas de autenticación
 
 export default app; // Exportar app para que pueda ser utilizado en otros archivos
